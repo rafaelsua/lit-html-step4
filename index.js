1780b1e73636b0a7bdebffde63bb563cdf99f182
rafaelsua <rafaelsua@gmail.com>
2020-02-11 16:18:04 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import {asyncReplace} from 'lit-html/directives/async-replace.js';
 import { render, html } from "lit-html";
 
-
+const wait = (t) => new Promise((resolve) => setTimeout(resolve, t));
+/**
+ * Returns an async iterable that yields increasing integers.
+ */
+async function* countUp() {
+  let i = 0;
+  while (true) {
+    yield i++;
+    await wait(1000);
+  }
+}
 
 render(html`
-  Count: <span>${asyncReplace()}</span>.
+  Count: <span>${asyncReplace(countUp())}</span>.
 `, document.body);
-
